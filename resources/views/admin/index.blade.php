7a5618fe8cd31bb6954c9f4fadcdd30445173d39
Evghenii Cerednicenko <agentche93@gmail.com>
2018-04-03 09:43:02 +0300

    Добавил возможность удаления аниме.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,20 +2,26 @@
 
 @section('content')
 
-<a href="{{ route('admin.create') }}" class="massive ui button">Add new anime!</a>
+    <a href="{{ route('admin.create') }}" class="massive ui button">Add new anime!</a>
 
-@forelse($animes as $anime)
-    <div class="thumbnail">
-        <img src="{{ $anime->image }}" alt="...">
-        <div class="caption">
-            <h4>"{{ $anime->title }}"</h4>
-            <cite><h4 align="left">{{ $anime->english_title }}</h4></cite>
-            <cite><h4 align="left">{{ $anime->id }}</h4></cite>
-            <p>Episode: {{ $anime->episodes }}</p>
+    @forelse($animes as $anime)
+        {{--    @method('delete')--}}
+        <div>
+            <img src="{{ $anime->image }}" alt="...">
+            <div class="caption">
+                <h4>"{{ $anime->title }}"</h4>
+                <cite><h4 align="left">{{ $anime->english_title }}</h4></cite>
+                <cite><h4 align="left">{{ $anime->id }}</h4></cite>
+                <p>Episode: {{ $anime->episodes }}</p>
+            </div>
         </div>
-    </div>
-    <a href="{{ route('admin.destroy', ['id' => $anime->id]) }}" class="ui button">Уничтожить</a>
-@empty
-@endforelse
-<div align="center">{!! $animes->render() !!}</div>
+        <form action="{{ route('admin.destroy', ['id' => $anime->id]) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit" class="ui button">Destroy!</button>
+        </form>
+        {{--    <a href="{{ route('admin.destroy', ['id' => $anime->id]) }}" class="ui button" data-method="delete" data-token="{{csrf_token()}}">Уничтожить</a>--}}
+    @empty
+    @endforelse
+    <div align="center">{!! $animes->render() !!}</div>
 @endsection
